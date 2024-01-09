aa971fbe2eb36ace2af88f9421dd387035d21231
Guilherme Pedro <guilherme_pedro12@hotmail.com>
2024-01-08 22:22:55 -0300

    implementando webconfig

--- src/main/java/com/example/usuario/api/controller/MedicoController.java
+++ src/main/java/com/example/usuario/api/controller/MedicoController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.usuario.api.model.Medico;
-import com.example.usuario.api.model.Paciente;
 import com.example.usuario.api.service.MedicoService;
 
 
--- src/main/java/com/example/usuario/api/model/Medico.java
+++ src/main/java/com/example/usuario/api/model/Medico.java
@@ -22,6 +22,60 @@ public class Medico {
 	String endereco;
 	String especializacao;
 	
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getNumeroCRM() {
+		return numeroCRM;
+	}
+
+
+	public void setNumeroCRM(String numeroCRM) {
+		this.numeroCRM = numeroCRM;
+	}
+
+
+	public String getPacientes() {
+		return pacientes;
+	}
+
+
+	public void setPacientes(String pacientes) {
+		this.pacientes = pacientes;
+	}
+
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+
+	public String getEspecializacao() {
+		return especializacao;
+	}
+
+
+	public void setEspecializacao(String especializacao) {
+		this.especializacao = especializacao;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	
 	public Long getId() {
 		return id;
